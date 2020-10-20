b4a2e6615ad946f954db1216c3bf5d0f3e666880
apstndb <gamaguchi@gmail.com>
2020-10-20 16:21:56 +0900

    Minor fix

--- astutil.go
+++ astutil.go
@@ -9,7 +9,7 @@ import (
 	"github.com/MakeNowJust/memefish/pkg/ast"
 	"github.com/MakeNowJust/memefish/pkg/parser"
 	"github.com/MakeNowJust/memefish/pkg/token"
-	spanner2 "google.golang.org/genproto/googleapis/spanner/v1"
+	spannerpb "google.golang.org/genproto/googleapis/spanner/v1"
 	"google.golang.org/protobuf/types/known/structpb"
 )
 
@@ -17,12 +17,12 @@ func astExprToGenericColumnValue(t ast.Expr) (spanner.GenericColumnValue, error)
 	switch t := t.(type) {
 	case *ast.NullLiteral:
 		return spanner.GenericColumnValue{
-			Type:  &spanner2.Type{Code: spanner2.TypeCode_INT64},
+			Type:  &spannerpb.Type{Code: spannerpb.TypeCode_INT64},
 			Value: structpb.NewNullValue(),
 		}, nil
 	case *ast.BoolLiteral:
 		return spanner.GenericColumnValue{
-			Type:  &spanner2.Type{Code: spanner2.TypeCode_BOOL},
+			Type:  &spannerpb.Type{Code: spannerpb.TypeCode_BOOL},
 			Value: structpb.NewBoolValue(t.Value),
 		}, nil
 	case *ast.IntLiteral:
@@ -31,7 +31,7 @@ func astExprToGenericColumnValue(t ast.Expr) (spanner.GenericColumnValue, error)
 			return spanner.GenericColumnValue{}, err
 		}
 		return spanner.GenericColumnValue{
-			Type:  &spanner2.Type{Code: spanner2.TypeCode_INT64},
+			Type:  &spannerpb.Type{Code: spannerpb.TypeCode_INT64},
 			Value: structpb.NewStringValue(strconv.FormatInt(i, 10)),
 		}, nil
 	case *ast.FloatLiteral:
@@ -40,37 +40,37 @@ func astExprToGenericColumnValue(t ast.Expr) (spanner.GenericColumnValue, error)
 			return spanner.GenericColumnValue{}, err
 		}
 		return spanner.GenericColumnValue{
-			Type:  &spanner2.Type{Code: spanner2.TypeCode_FLOAT64},
+			Type:  &spannerpb.Type{Code: spannerpb.TypeCode_FLOAT64},
 			Value: structpb.NewNumberValue(i),
 		}, nil
 	case *ast.StringLiteral:
 		return spanner.GenericColumnValue{
-			Type:  &spanner2.Type{Code: spanner2.TypeCode_STRING},
+			Type:  &spannerpb.Type{Code: spannerpb.TypeCode_STRING},
 			Value: structpb.NewStringValue(t.Value),
 		}, nil
 	case *ast.BytesLiteral:
 		return spanner.GenericColumnValue{
-			Type:  &spanner2.Type{Code: spanner2.TypeCode_BYTES},
+			Type:  &spannerpb.Type{Code: spannerpb.TypeCode_BYTES},
 			Value: structpb.NewStringValue(base64.StdEncoding.EncodeToString(t.Value)),
 		}, nil
 	case *ast.DateLiteral:
 		return spanner.GenericColumnValue{
-			Type:  &spanner2.Type{Code: spanner2.TypeCode_DATE},
+			Type:  &spannerpb.Type{Code: spannerpb.TypeCode_DATE},
 			Value: structpb.NewStringValue(t.Value.Value),
 		}, nil
 	case *ast.TimestampLiteral:
 		return spanner.GenericColumnValue{
-			Type:  &spanner2.Type{Code: spanner2.TypeCode_TIMESTAMP},
+			Type:  &spannerpb.Type{Code: spannerpb.TypeCode_TIMESTAMP},
 			Value: structpb.NewStringValue(t.Value.Value),
 		}, nil
 	case *ast.NumericLiteral:
 		return spanner.GenericColumnValue{
-			Type:  &spanner2.Type{Code: spanner2.TypeCode_NUMERIC},
+			Type:  &spannerpb.Type{Code: spannerpb.TypeCode_NUMERIC},
 			Value: structpb.NewStringValue(t.Value.Value),
 		}, nil
 	case *ast.ArrayLiteral:
 		var values []*structpb.Value
-		var typ *spanner2.Type
+		var typ *spannerpb.Type
 		for _, v := range t.Values {
 			value, err := astExprToGenericColumnValue(v)
 			if err != nil {
@@ -92,11 +92,11 @@ func astExprToGenericColumnValue(t ast.Expr) (spanner.GenericColumnValue, error)
 			typ = value.Type
 		}
 		return spanner.GenericColumnValue{
-			Type:  &spanner2.Type{ArrayElementType: typ, Code: spanner2.TypeCode_ARRAY},
+			Type:  &spannerpb.Type{ArrayElementType: typ, Code: spannerpb.TypeCode_ARRAY},
 			Value: structpb.NewListValue(&structpb.ListValue{Values: values}),
 		}, nil
 	case *ast.StructLiteral:
-		var fields []*spanner2.StructType_Field
+		var fields []*spannerpb.StructType_Field
 		var values []*structpb.Value
 		for i, v := range t.Values {
 			genValue, err := astExprToGenericColumnValue(v)
@@ -104,7 +104,7 @@ func astExprToGenericColumnValue(t ast.Expr) (spanner.GenericColumnValue, error)
 				return spanner.GenericColumnValue{}, err
 			}
 			var name string
-			var typ *spanner2.Type
+			var typ *spannerpb.Type
 			if len(t.Fields) > i {
 				field := t.Fields[i]
 				if field.Ident != nil {
@@ -121,16 +121,16 @@ func astExprToGenericColumnValue(t ast.Expr) (spanner.GenericColumnValue, error)
 			if typ == nil {
 				typ = genValue.Type
 			}
-			fields = append(fields, &spanner2.StructType_Field{
+			fields = append(fields, &spannerpb.StructType_Field{
 				Name: name,
 				Type: typ,
 			})
 			values = append(values, genValue.Value)
 		}
 		return spanner.GenericColumnValue{
-			Type: &spanner2.Type{
-				StructType: &spanner2.StructType{Fields: fields},
-				Code:       spanner2.TypeCode_STRUCT},
+			Type: &spannerpb.Type{
+				StructType: &spannerpb.StructType{Fields: fields},
+				Code:       spannerpb.TypeCode_STRUCT},
 			Value: structpb.NewListValue(&structpb.ListValue{Values: values}),
 		}, nil
 	default:
@@ -150,12 +150,12 @@ func astTypeToGenericColumnValue(t ast.Type) (spanner.GenericColumnValue, error)
 	return spanner.GenericColumnValue{Type: typ, Value: v}, nil
 }
 
-func astTypeToSpannerpbType(t ast.Type) (*spanner2.Type, error) {
+func astTypeToSpannerpbType(t ast.Type) (*spannerpb.Type, error) {
 	switch t := t.(type) {
 	case *ast.SimpleType:
 		return astSimpleTypeToSpannerpbType(t)
 	case *ast.ArrayType:
-		var typ *spanner2.Type
+		var typ *spannerpb.Type
 		if t.Item != nil {
 			var err error
 			typ, err = astTypeToSpannerpbType(t.Item)
@@ -165,9 +165,9 @@ func astTypeToSpannerpbType(t ast.Type) (*spanner2.Type, error) {
 		} else {
 			return nil, fmt.Errorf("t is unknown")
 		}
-		return &spanner2.Type{ArrayElementType: typ, Code: spanner2.TypeCode_ARRAY}, nil
+		return &spannerpb.Type{ArrayElementType: typ, Code: spannerpb.TypeCode_ARRAY}, nil
 	case *ast.StructType:
-		var fields []*spanner2.StructType_Field
+		var fields []*spannerpb.StructType_Field
 		for _, f := range t.Fields {
 			t, err := astTypeToSpannerpbType(f.Type)
 			if err != nil {
@@ -177,38 +177,38 @@ func astTypeToSpannerpbType(t ast.Type) (*spanner2.Type, error) {
 			if f.Ident != nil {
 				name = f.Ident.Name
 			}
-			fields = append(fields, &spanner2.StructType_Field{
+			fields = append(fields, &spannerpb.StructType_Field{
 				Name: name,
 				Type: t,
 			})
 		}
-		return &spanner2.Type{StructType: &spanner2.StructType{Fields: fields}, Code: spanner2.TypeCode_STRUCT}, nil
+		return &spannerpb.Type{StructType: &spannerpb.StructType{Fields: fields}, Code: spannerpb.TypeCode_STRUCT}, nil
 	default:
 		return nil, fmt.Errorf("not implemented: %s", t.SQL())
 	}
 }
 
-func astSimpleTypeToSpannerpbType(t *ast.SimpleType) (*spanner2.Type, error) {
+func astSimpleTypeToSpannerpbType(t *ast.SimpleType) (*spannerpb.Type, error) {
 	if t == nil {
 		return nil, fmt.Errorf("t is nil")
 	}
 	switch t.Name {
 	case ast.BoolTypeName:
-		return &spanner2.Type{Code: spanner2.TypeCode_BOOL}, nil
+		return &spannerpb.Type{Code: spannerpb.TypeCode_BOOL}, nil
 	case ast.Int64TypeName:
-		return &spanner2.Type{Code: spanner2.TypeCode_INT64}, nil
+		return &spannerpb.Type{Code: spannerpb.TypeCode_INT64}, nil
 	case ast.Float64TypeName:
-		return &spanner2.Type{Code: spanner2.TypeCode_FLOAT64}, nil
+		return &spannerpb.Type{Code: spannerpb.TypeCode_FLOAT64}, nil
 	case ast.StringTypeName:
-		return &spanner2.Type{Code: spanner2.TypeCode_STRING}, nil
+		return &spannerpb.Type{Code: spannerpb.TypeCode_STRING}, nil
 	case ast.BytesTypeName:
-		return &spanner2.Type{Code: spanner2.TypeCode_BYTES}, nil
+		return &spannerpb.Type{Code: spannerpb.TypeCode_BYTES}, nil
 	case ast.DateTypeName:
-		return &spanner2.Type{Code: spanner2.TypeCode_DATE}, nil
+		return &spannerpb.Type{Code: spannerpb.TypeCode_DATE}, nil
 	case ast.TimestampTypeName:
-		return &spanner2.Type{Code: spanner2.TypeCode_TIMESTAMP}, nil
+		return &spannerpb.Type{Code: spannerpb.TypeCode_TIMESTAMP}, nil
 	case ast.NumericTypeName:
-		return &spanner2.Type{Code: spanner2.TypeCode_NUMERIC}, nil
+		return &spannerpb.Type{Code: spannerpb.TypeCode_NUMERIC}, nil
 	default:
 		return nil, fmt.Errorf("t.Name is unknown: %s", t.Name)
 	}
