33deede99dfdcc17618c4a3a9b48a4b44e5282db
apstndb <gamaguchi@gmail.com>
2020-10-23 12:06:17 +0900

    Use SDK supplied Metadata

--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/apstndb/execspansql
 go 1.15
 
 require (
-	cloud.google.com/go/spanner v1.10.0
+	cloud.google.com/go/spanner v1.11.1-0.20201022172608-846989f5b66c
 	github.com/MakeNowJust/memefish v0.0.0-20201019133624-0b597d4af7d9
 	github.com/goccy/go-json v0.1.13
 	github.com/goccy/go-yaml v1.8.2
@@ -11,9 +11,12 @@ require (
 	github.com/itchyny/gojq v0.11.2
 	github.com/jessevdk/go-flags v1.4.0
 	go.uber.org/zap v1.16.0
-	google.golang.org/api v0.31.0
-	google.golang.org/genproto v0.0.0-20200904004341-0bd0a958aa1d
-	google.golang.org/grpc v1.31.1
+	golang.org/x/net v0.0.0-20201022231255-08b38378de70 // indirect
+	golang.org/x/sys v0.0.0-20201022201747-fb209a7c41cd // indirect
+	golang.org/x/tools v0.0.0-20201022215848-ffe8bce740af // indirect
+	google.golang.org/api v0.33.0
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/genproto v0.0.0-20201022181438-0ff5f38871d5
+	google.golang.org/grpc v1.33.1
 	google.golang.org/protobuf v1.25.0
-	gopkg.in/yaml.v2 v2.2.8
 )
--- main.go
+++ main.go
@@ -272,16 +272,9 @@ func consumeRowIter(rowIter *spanner.RowIterator, redactRows bool) (*spannerpb.R
 
 func convertToResultSet(consumeResult *consumeRowIterResult) (*spannerpb.ResultSet, error) {
 	// Leave null if fields are not populated
-	var metadata *spannerpb.ResultSetMetadata
-	if consumeResult.RowType != nil {
-		metadata = &spannerpb.ResultSetMetadata{
-			RowType: &spannerpb.StructType{Fields: consumeResult.RowType},
-		}
-	}
-
 	rs := &spannerpb.ResultSet{
 		Rows:     consumeResult.Rows,
-		Metadata: metadata,
+		Metadata: consumeResult.Metadata,
 	}
 
 	var queryStats *structpb.Struct
@@ -308,25 +301,17 @@ func convertToResultSet(consumeResult *consumeRowIterResult) (*spannerpb.ResultS
 }
 
 type consumeRowIterResult struct {
-	RowType    []*spannerpb.StructType_Field
-	Rows       []*structpb.ListValue
+	Metadata   *spannerpb.ResultSetMetadata
 	QueryPlan  *spannerpb.QueryPlan
 	QueryStats map[string]interface{}
 	RowCount   int64
+	Rows       []*structpb.ListValue
 }
 
 func consumeRowIterImpl(rowIter *spanner.RowIterator, redactRows bool) (*consumeRowIterResult, error) {
-	var rowType []*spannerpb.StructType_Field
 	var rows []*structpb.ListValue
 	err := rowIter.Do(func(r *spanner.Row) error {
 		var err error
-		if rowType == nil {
-			rowType, err = extractRowType(r)
-			if err != nil {
-				return err
-			}
-		}
-
 		if redactRows {
 			return nil
 		}
@@ -343,11 +328,11 @@ func consumeRowIterImpl(rowIter *spanner.RowIterator, redactRows bool) (*consume
 		return nil, err
 	}
 	return &consumeRowIterResult{
-		RowType:    rowType,
 		Rows:       rows,
 		QueryPlan:  rowIter.QueryPlan,
 		QueryStats: rowIter.QueryStats,
 		RowCount:   rowIter.RowCount,
+		Metadata:   rowIter.Metadata,
 	}, nil
 }
 
