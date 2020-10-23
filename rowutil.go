ffa98caec7d0c25b77b2c7858c1cb865d002ed94
apstndb <gamaguchi@gmail.com>
2020-10-23 14:51:42 +0900

    Remote unused function

--- rowutil.go
+++ rowutil.go
@@ -2,25 +2,8 @@ package main
 
 import (
 	"cloud.google.com/go/spanner"
-	spannerpb "google.golang.org/genproto/googleapis/spanner/v1"
 	"google.golang.org/protobuf/types/known/structpb"
 )
-
-func extractRowType(r *spanner.Row) ([]*spannerpb.StructType_Field, error) {
-	var rowType []*spannerpb.StructType_Field
-	for i, name := range r.ColumnNames() {
-		var v spanner.GenericColumnValue
-		if err := r.Column(i, &v); err != nil {
-			return nil, err
-		}
-
-		rowType = append(rowType, &spannerpb.StructType_Field{
-			Name: name,
-			Type: v.Type,
-		})
-	}
-	return rowType, nil
-}
 
 func rowValues(r *spanner.Row) ([]*structpb.Value, error) {
 	var vs []*structpb.Value
