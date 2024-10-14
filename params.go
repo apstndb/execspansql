748ca06eb339cd972edd27b0be540fabea5d77f3
apstndb <803393+apstndb@users.noreply.github.com>
2024-10-14 19:51:14 +0900

    Refactor generateParam

--- params.go
+++ params.go
@@ -4,40 +4,48 @@ import (
 	"fmt"
 
 	"cloud.google.com/go/spanner"
-	"cloud.google.com/go/spanner/apiv1/spannerpb"
-	"github.com/cloudspannerecosystem/memefish/ast"
-	"google.golang.org/protobuf/types/known/structpb"
 )
 
 func generateParams(ss map[string]string, permitType bool) (map[string]interface{}, error) {
 	result := make(map[string]interface{})
 	for name, code := range ss {
-		var err error
-		var value spanner.GenericColumnValue
-		var typ ast.Type
-		var expr ast.Expr
-
-		if typ, err = parseType(code); permitType && err == nil {
-			debuglog.Println(name, "ast.Type.SQL():", typ.SQL())
-			value, err = astTypeToGenericColumnValue(typ)
-		} else if permitType && code == "JSON" {
-			// workaround for JSON type(not literal)
-			value = spanner.GenericColumnValue{Type: &spannerpb.Type{Code: spannerpb.TypeCode_JSON}, Value: structpb.NewNullValue()}
-			// reset err
-			err = nil
-		} else if expr, err = parseExpr(code); err == nil {
-			debuglog.Println(name, "ast.Expr.SQL():", expr.SQL())
-			value, err = astExprToGenericColumnValue(expr)
-		} else {
-			return nil, fmt.Errorf("error on parsing param `%s`: %w", name, err)
-		}
-
+		value, err := generateParam(code, permitType)
 		if err != nil {
-			return nil, fmt.Errorf("error on processing param `%s`: %w", name, err)
+			return nil, fmt.Errorf("error on %v: %w", name, err)
 		}
-
-		debuglog.Println(name, "spannerpb.Type:", value.Type)
 		result[name] = value
 	}
 	return result, nil
+}
+
+func generateParam(code string, permitType bool) (spanner.GenericColumnValue, error) {
+	if permitType {
+		typ, err := parseType(code)
+		if err == nil {
+			debuglog.Println("ast.Type.SQL():", typ.SQL())
+			value, err := astTypeToGenericColumnValue(typ)
+			if err != nil {
+				return spanner.GenericColumnValue{}, fmt.Errorf("error on generating value for type: %w", err)
+			}
+			return value, err
+		}
+		// ignore parse err
+		debuglog.Println("ignore parse error:", err)
+	}
+
+	expr, err := parseExpr(code)
+	if err != nil {
+		return spanner.GenericColumnValue{}, fmt.Errorf("error on parsing expr: %w", err)
+	}
+
+	debuglog.Println("ast.Expr.SQL():", expr.SQL())
+
+	value, err := astExprToGenericColumnValue(expr)
+
+	if err != nil {
+		return spanner.GenericColumnValue{}, fmt.Errorf("error on generating value: %w", err)
+	}
+
+	debuglog.Println("spannerpb.Type:", value.Type)
+	return value, nil
 }
