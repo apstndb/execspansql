faa13610500228157b61a9e3123f8170b1a8a472
apstndb <803393+apstndb@users.noreply.github.com>
2024-10-17 03:25:25 +0900

    Refactor consumeRowIter

--- util.go
+++ util.go
@@ -18,6 +18,7 @@ func tupled[T1, T2, R any](f func(T1, T2) R) func(xiter.Zipped[T1, T2]) R {
 		return f(z.V1, z.V2)
 	}
 }
+
 func tupledWithErr[T1, T2, R any](f func(T1, T2) (R, error)) func(xiter.Zipped[T1, T2]) (R, error) {
 	return func(z xiter.Zipped[T1, T2]) (R, error) {
 		return f(z.V1, z.V2)
@@ -29,7 +30,7 @@ func tryJoin(delegate iter.Seq2[string, error], sep string) (string, error) {
 	var b strings.Builder
 	for s, err := range delegate {
 		if err != nil {
-			return "", err
+			return b.String(), err
 		}
 		if !first {
 			b.WriteString(sep)
@@ -42,13 +43,33 @@ func tryJoin(delegate iter.Seq2[string, error], sep string) (string, error) {
 }
 
 func tryMapMap[K comparable, T, R any](m map[K]T, f func(K, T) (R, error)) (map[K]R, error) {
-	res := make(map[K]R)
+	res := make(map[K]R, len(m))
 	for k, v := range m {
 		newV, err := f(k, v)
 		if err != nil {
-			return nil, err
+			return res, err
 		}
 		res[k] = newV
 	}
 	return res, nil
+}
+
+// mapNonError apply function to the first value of pair in iter.Seq2.
+// It passes through values if error isn't nil, and the first value become zero value of the return type of f.
+func mapNonError[T, R any](delegate iter.Seq2[T, error], f func(T) R) iter.Seq2[R, error] {
+	return func(yield func(R, error) bool) {
+		for v, err := range delegate {
+			if err != nil {
+				ok := yield(lo.Empty[R](), err)
+				if !ok {
+					return
+				}
+				continue
+			}
+			ok := yield(f(v), err)
+			if !ok {
+				return
+			}
+		}
+	}
 }
